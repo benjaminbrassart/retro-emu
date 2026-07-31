abab6788ab7ae8aef4654b4a85c2c7d4efe86270
Benjamin Brassart <bbrassar@student.42.fr>
2026-07-31 16:26:13 +0200

    Add `Cartridge::load` prototype

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::io;
+
 pub struct Cartridge {
     pub rom: Box<[u8]>,
     pub ram: Box<[u8]>,
+}
+
+impl Cartridge {
+    pub fn load<R: io::Read>(_r: R) -> io::Result<Self> {
+        todo!("load cartridge from reader")
+    }
 }
 
 fn main() {
