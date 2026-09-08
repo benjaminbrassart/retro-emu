1c74cd3a00a9501fa45eaae6b271ba85a11baac9
Benjamin Brassart <bbrassar@student.42.fr>
2026-09-08 13:10:33 +0200

    Add getters and setters for 16-bit registers

--- src/cpu.rs
+++ src/cpu.rs
@@ -13,4 +13,47 @@ pub struct Cpu {
 }
 
 impl Cpu {
+    pub fn get_af(&self) -> u16 {
+        u16::from_be_bytes([self.a, self.f])
+    }
+
+    pub fn set_af(&mut self, value: u16) {
+        let [a, f] = value.to_be_bytes();
+
+        self.a = a;
+        self.f = f & 0xf0;
+    }
+
+    pub fn get_bc(&self) -> u16 {
+        u16::from_be_bytes([self.b, self.c])
+    }
+
+    pub fn set_bc(&mut self, value: u16) {
+        let [b, c] = value.to_be_bytes();
+
+        self.b = b;
+        self.c = c;
+    }
+
+    pub fn get_de(&self) -> u16 {
+        u16::from_be_bytes([self.d, self.e])
+    }
+
+    pub fn set_de(&mut self, value: u16) {
+        let [d, e] = value.to_be_bytes();
+
+        self.d = d;
+        self.e = e;
+    }
+
+    pub fn get_hl(&self) -> u16 {
+        u16::from_be_bytes([self.h, self.l])
+    }
+
+    pub fn set_hl(&mut self, value: u16) {
+        let [h, l] = value.to_be_bytes();
+
+        self.h = h;
+        self.l = l;
+    }
 }
