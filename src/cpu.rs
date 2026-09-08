2ea8819fa31efd2f896c1a826776cc3246ec6945
Benjamin Brassart <bbrassar@student.42.fr>
2026-09-08 14:27:36 +0200

    Add most of 8-bit LD instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -144,7 +144,187 @@ impl Cpu {
             0x76 => Instruction::Halt,
             0xf3 => Instruction::DisableInterrupts,
             0xfb => Instruction::EnableInterrupts,
+            0x02 | 0x12 | 0x22 | 0x32 => {
+                let dst = match opcode {
+                    0x02 => Dst8::AtReg16(Reg16::BC),
+                    0x12 => Dst8::AtReg16(Reg16::DE),
+                    0x22 => Dst8::AtHLI,
+                    0x32 => Dst8::AtHLD,
+                    _ => unreachable!(),
+                };
+
+                Instruction::Load8 {
+                    src: Src8::Reg8(Reg8::A),
+                    dst,
+                }
+            }
+            0x06 | 0x16 | 0x26 | 0x36 => {
+                let dst = match opcode {
+                    0x06 => Dst8::Reg8(Reg8::B),
+                    0x16 => Dst8::Reg8(Reg8::D),
+                    0x26 => Dst8::Reg8(Reg8::H),
+                    0x36 => Dst8::AtReg16(Reg16::HL),
+                    _ => unreachable!(),
+                };
+                let src = Src8::Imm8(self.fetch_next_byte(bus));
+
+                Instruction::Load8 { src, dst }
+            }
+            0x0a | 0x1a | 0x2a | 0x3a => {
+                let src = match opcode {
+                    0x0a => Src8::AtReg16(Reg16::BC),
+                    0x1a => Src8::AtReg16(Reg16::DE),
+                    0x2a => Src8::AtHLI,
+                    0x3a => Src8::AtHLD,
+                    _ => unreachable!(),
+                };
+
+                Instruction::Load8 {
+                    src,
+                    dst: Dst8::Reg8(Reg8::A),
+                }
+            }
+            0x0e | 0x1e | 0x2e | 0x3e => {
+                let dst = match opcode {
+                    0x0e => Dst8::Reg8(Reg8::C),
+                    0x1e => Dst8::Reg8(Reg8::E),
+                    0x2e => Dst8::Reg8(Reg8::L),
+                    0x3e => Dst8::Reg8(Reg8::A),
+                    _ => unreachable!(),
+                };
+                let src = Src8::Imm8(self.fetch_next_byte(bus));
+
+                Instruction::Load8 { src, dst }
+            }
+            0x40..0x76 | 0x77..0x80 => {
+                let src = match opcode & 0b0000_0111 {
+                    0b0000_0000 => Src8::Reg8(Reg8::B),
+                    0b0000_0001 => Src8::Reg8(Reg8::C),
+                    0b0000_0010 => Src8::Reg8(Reg8::D),
+                    0b0000_0011 => Src8::Reg8(Reg8::E),
+                    0b0000_0100 => Src8::Reg8(Reg8::H),
+                    0b0000_0101 => Src8::Reg8(Reg8::L),
+                    0b0000_0110 => Src8::AtReg16(Reg16::HL),
+                    0b0000_0111 => Src8::Reg8(Reg8::A),
+                    _ => unreachable!(),
+                };
+
+                let dst = match (opcode & 0b0011_1000) >> 3 {
+                    0b0000_0000 => Dst8::Reg8(Reg8::B),
+                    0b0000_0001 => Dst8::Reg8(Reg8::C),
+                    0b0000_0010 => Dst8::Reg8(Reg8::D),
+                    0b0000_0011 => Dst8::Reg8(Reg8::E),
+                    0b0000_0100 => Dst8::Reg8(Reg8::H),
+                    0b0000_0101 => Dst8::Reg8(Reg8::L),
+                    0b0000_0110 => Dst8::AtReg16(Reg16::HL),
+                    0b0000_0111 => Dst8::Reg8(Reg8::A),
+                    _ => unreachable!(),
+                };
+
+                Instruction::Load8 { src, dst }
+            }
             _ => todo!("unhandled instruction: {opcode:#04x}"),
+        }
+    }
+
+    pub fn handle_instruction<B>(&mut self, _: &mut B, _: Instruction) -> usize
+    where
+        B: Bus,
+    {
+        todo!()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Reg8 {
+    A,
+    F,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+}
+
+impl std::fmt::Display for Reg8 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::A => write!(f, "A"),
+            Self::F => write!(f, "F"),
+            Self::B => write!(f, "B"),
+            Self::C => write!(f, "C"),
+            Self::D => write!(f, "D"),
+            Self::E => write!(f, "E"),
+            Self::H => write!(f, "H"),
+            Self::L => write!(f, "L"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Reg16 {
+    AF,
+    BC,
+    DE,
+    HL,
+    PC,
+    SP,
+}
+
+impl std::fmt::Display for Reg16 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::AF => write!(f, "AF"),
+            Self::BC => write!(f, "BC"),
+            Self::DE => write!(f, "DE"),
+            Self::HL => write!(f, "HL"),
+            Self::PC => write!(f, "PC"),
+            Self::SP => write!(f, "SP"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Src8 {
+    Reg8(Reg8),
+    Imm8(u8),
+    AtReg16(Reg16),
+    AtImm16(u16),
+    AtHLI,
+    AtHLD,
+}
+
+impl std::fmt::Display for Src8 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::Reg8(reg) => write!(f, "{reg}"),
+            Self::Imm8(b) => write!(f, "{b:#04x}"),
+            Self::AtReg16(reg) => write!(f, "[{reg}]"),
+            Self::AtImm16(addr) => write!(f, "[{addr:#06x}]"),
+            Self::AtHLI => write!(f, "[{}+]", Reg16::HL),
+            Self::AtHLD => write!(f, "[{}-]", Reg16::HL),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Dst8 {
+    Reg8(Reg8),
+    AtReg16(Reg16),
+    AtImm16(u16),
+    AtHLI,
+    AtHLD,
+}
+
+impl std::fmt::Display for Dst8 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::Reg8(reg) => write!(f, "{reg}"),
+            Self::AtReg16(reg) => write!(f, "[{reg}]"),
+            Self::AtImm16(addr) => write!(f, "[{addr:#06x}]"),
+            Self::AtHLI => write!(f, "[{}+]", Reg16::HL),
+            Self::AtHLD => write!(f, "[{}-]", Reg16::HL),
         }
     }
 }
@@ -157,6 +337,7 @@ pub enum Instruction {
     Halt,
     EnableInterrupts,
     DisableInterrupts,
+    Load8 { dst: Dst8, src: Src8 },
 }
 
 impl std::fmt::Display for Instruction {
@@ -168,6 +349,7 @@ impl std::fmt::Display for Instruction {
             Self::Halt => write!(f, "HALT"),
             Self::EnableInterrupts => write!(f, "EI"),
             Self::DisableInterrupts => write!(f, "DI"),
+            Self::Load8 { dst, src } => write!(f, "LD {dst}, {src}"),
         }
     }
 }
@@ -204,7 +386,11 @@ mod tests {
     fn decode_nop_multiple() {
         let mut cpu = Cpu::default();
         let mut bus = TestBus {
-            data: vec![0x00, 0x00, 0x00],
+            data: vec![
+                0x00, // NOP
+                0x00, // NOP
+                0x00, // NOP
+            ],
         };
 
         assert_eq!(cpu.fetch_next_instruction(&mut bus), Instruction::Nop);
@@ -216,13 +402,76 @@ mod tests {
     fn decode_mixed() {
         let mut cpu = Cpu::default();
         let mut bus = TestBus {
-            data: vec![0x10, 0x00, 0xfb, 0x00, 0x10, 0x10, 0xf3 ],
+            data: vec![
+                0x10, 0x00, // STOP 0x00
+                0xfb, // EI
+                0x00, // NOP
+                0x10, 0x10, // STOP 0x10
+                0xf3, // DI
+            ],
         };
 
-        assert_eq!(cpu.fetch_next_instruction(&mut bus), Instruction::Stop { code: 0x00 });
-        assert_eq!(cpu.fetch_next_instruction(&mut bus), Instruction::EnableInterrupts);
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::Stop { code: 0x00 }
+        );
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::EnableInterrupts
+        );
         assert_eq!(cpu.fetch_next_instruction(&mut bus), Instruction::Nop);
-        assert_eq!(cpu.fetch_next_instruction(&mut bus), Instruction::Stop { code: 0x10 });
-        assert_eq!(cpu.fetch_next_instruction(&mut bus), Instruction::DisableInterrupts);
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::Stop { code: 0x10 }
+        );
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::DisableInterrupts
+        );
+    }
+
+    #[test]
+    fn decode_ld8() {
+        let mut cpu = Cpu::default();
+        let mut bus = TestBus {
+            data: vec![
+                0x7f, // LD A, A
+                0x7f, // LD A, A
+                0x3e, 0x42, // LD A, 0x42
+                0x22, // LD [DE], A
+            ],
+        };
+
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::Load8 {
+                dst: Dst8::Reg8(Reg8::A),
+                src: Src8::Reg8(Reg8::A),
+            },
+        );
+
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::Load8 {
+                dst: Dst8::Reg8(Reg8::A),
+                src: Src8::Reg8(Reg8::A),
+            },
+        );
+
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::Load8 {
+                dst: Dst8::Reg8(Reg8::A),
+                src: Src8::Imm8(0x42),
+            },
+        );
+
+        assert_eq!(
+            cpu.fetch_next_instruction(&mut bus),
+            Instruction::Load8 {
+                dst: Dst8::AtReg16(Reg16::DE),
+                src: Src8::Reg8(Reg8::A),
+            },
+        );
     }
 }
